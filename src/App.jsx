293f3cddc199bc9102fa87e48a4184dbb4b08dec
Juan Vallejo <bagy45@gmail.com>
2025-09-13 22:31:18 -0400

    Remove unused image assets and update App component to handle mortgage calculations more effectively. Added total repayment calculation and improved form handling for better user experience.

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ function App() {
     interestRate: "",
     mortgageType: "",
   });
-  const [results, setResults] = useState(null);
+  const [results, setResults] = useState(0);
+  const [totalRepayment, setTotalRepayment] = useState(0);
   const [hasResults, setHasResults] = useState(true);
 
   function handleChange(e) {
-    const { name, value, type, checked } = e.target;
-
+    const { name, value } = e.target;
     setFormData((prev) => ({
       ...prev,
       [name]: value,
@@ -32,171 +32,198 @@ function App() {
     const principal = Number(formData.mortgageAmt);
     const annualRate = Number(formData.interestRate) / 100;
     const monthlyRepayment = (principal * annualRate) / 12;
-    setResults(monthlyRepayment);
+    const years = Number(formData.mortgageTerm);
+    const totalRepayment = monthlyRepayment * years * 12 + principal;
     setHasResults(true);
+    setResults(monthlyRepayment);
+    setTotalRepayment(totalRepayment);
   }
 
   function calculateRepayment() {
-    const principal = Number(formData.mortgageAmt); // P
-    const annualRate = Number(formData.interestRate); // in %
-    const years = Number(formData.mortgageTerm); // in years
-
-    const r = annualRate / 100 / 12; // monthly interest rate
-    const n = years * 12; // total months
+    const principal = Number(formData.mortgageAmt);
+    const annualRate = Number(formData.interestRate);
+    const years = Number(formData.mortgageTerm);
+    const r = annualRate / 100 / 12;
+    const n = years * 12;
     const monthlyRepayment =
       (principal * (r * Math.pow(1 + r, n))) / (Math.pow(1 + r, n) - 1);
     setResults(monthlyRepayment);
+    setTotalRepayment(monthlyRepayment * n);
     setHasResults(true);
   }
 
+  function clearAll() {
+    setFormData({
+      mortgageAmt: "",
+      mortgageTerm: "",
+      interestRate: "",
+      mortgageType: "",
+    });
+    setResults(null);
+    setTotalRepayment(0);
+    setHasResults(false);
+  }
+
   return (
-    <div>
-      <form className="flex flex-col gap-6 py-8 px-6" onSubmit={calculate}>
-        <div id="title-bar" className="flex flex-col gap-2 text-black mb-6">
-          <h1 className="font-bold text-2xl text-slate-900">
-            Mortgage Calculator
-          </h1>
-          <p className="text-base text-slate-700 underline cursor-pointer">
-            Clear All
-          </p>
-        </div>
-
-        <div
-          id="fields"
-          className="text-slate-700 flex flex-col gap-6 justify-between w-full"
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <div className="w-full max-w-6xl bg-white rounded-xl shadow-lg grid lg:grid-cols-2 md:gap-10 lg:gap-16 md:p-10">
+        <form
+          className="flex flex-col gap-6 p-6 md:p-0 h-full overflow-y-auto"
+          onSubmit={calculate}
         >
-          <div className="flex flex-col gap-3">
-            <label
-              htmlFor="mortgage-amt"
-              className="font-medium"
-              name="mortgageAmt"
+          <div className="flex flex-col gap-2 text-black mb-4">
+            <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl text-slate-900">
+              Mortgage Calculator
+            </h1>
+            <p
+              className="text-base text-slate-700 underline cursor-pointer"
+              onClick={clearAll}
             >
-              Mortgage Amount
-            </label>
-            <div className="flex border rounded-md w-full h-12">
-              <span className="bg-sky-100 rounded-l-md h-full flex items-center px-4 font-bold">
-                £
-              </span>
-              <input
-                type="number"
-                name="mortgageAmt"
-                className="flex-1 outline-none appearance-none h-full px-3"
-                onChange={handleChange}
-              />
-            </div>
+              Clear All
+            </p>
           </div>
 
-          <div className="flex flex-col gap-3">
-            <label htmlFor="mortgage-term" className="font-medium">
-              Mortgage Term
-            </label>
-            <div className="flex border rounded-md w-full h-12">
-              <input
-                type="number"
-                className="flex-1 outline-none appearance-none h-full px-3"
-                name="mortgageTerm"
-                onChange={handleChange}
-              />
-              <span className="bg-sky-100 rounded-r-md h-full flex items-center px-4 font-bold">
-                years
-              </span>
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-3">
-            <label htmlFor="mortgage-rate" className="font-medium">
-              Interest Rate
-            </label>
-            <div className="flex border rounded-md w-full h-12">
-              <input
-                type="number"
-                className="flex-1 outline-none appearance-none h-full px-3"
-                name="interestRate"
-                onChange={handleChange}
-              />
-              <span className="bg-sky-100 rounded-r-md h-full flex items-center px-4 font-bold">
-                %
-              </span>
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-3">
-            <label htmlFor="mortgageType" className="font-medium">
-              Mortgage Type
-            </label>
-            <div className="flex border rounded-md w-full items-center h-12 px-4 gap-4">
-              <input
-                type="radio"
-                value="repayment"
-                name="mortgageType"
-                id="repayment"
-                onChange={handleChange}
-              />
-              <label htmlFor="repayment" className="font-bold cursor-pointer">
-                Repayment
+          <div className="text-slate-700 flex flex-col gap-6">
+            <div className="flex flex-col gap-3">
+              <label htmlFor="mortgage-amt" className="font-medium">
+                Mortgage Amount
               </label>
+              <div className="flex border rounded-md w-full h-12">
+                <span className="bg-sky-100 rounded-l-md h-full flex items-center px-4 font-bold">
+                  £
+                </span>
+                <input
+                  type="number"
+                  name="mortgageAmt"
+                  className="flex-1 outline-none appearance-none h-full px-3"
+                  value={formData.mortgageAmt}
+                  onChange={handleChange}
+                />
+              </div>
             </div>
-            <div className="flex border rounded-md w-full items-center h-12 px-4 gap-4">
-              <input
-                type="radio"
-                value="interest-only"
-                name="mortgageType"
-                id="interest-only"
-                onChange={handleChange}
-              />
-              <label
-                htmlFor="interest-only"
-                className="font-bold cursor-pointer"
-              >
-                Interest Only
-              </label>
-            </div>
-          </div>
 
-          <button
-            type="submit"
-            className="flex items-center text-lg font-bold justify-center cursor-pointer bg-[#D8DB2F] rounded-full py-4 text-black"
-          >
+            <div className="flex flex-col md:flex-row gap-6 flex-wrap">
+              <div className="flex-1 flex flex-col gap-3">
+                <label htmlFor="mortgage-term" className="font-medium">
+                  Mortgage Term
+                </label>
+                <div className="flex border rounded-md w-full h-12">
+                  <input
+                    type="number"
+                    className="flex-1 outline-none appearance-none h-full px-3"
+                    name="mortgageTerm"
+                    value={formData.mortgageTerm}
+                    onChange={handleChange}
+                  />
+                  <span className="bg-sky-100 rounded-r-md h-full flex items-center px-4 font-bold">
+                    years
+                  </span>
+                </div>
+              </div>
+
+              <div className="flex-1 flex flex-col gap-3">
+                <label htmlFor="mortgage-rate" className="font-medium">
+                  Interest Rate
+                </label>
+                <div className="flex border rounded-md w-full h-12">
+                  <input
+                    type="number"
+                    className="flex-1 outline-none appearance-none h-full px-3"
+                    name="interestRate"
+                    value={formData.interestRate}
+                    onChange={handleChange}
+                  />
+                  <span className="bg-sky-100 rounded-r-md h-full flex items-center px-4 font-bold">
+                    %
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-3">
+              <label htmlFor="mortgageType" className="font-medium">
+                Mortgage Type
+              </label>
+              <div className="flex border rounded-md w-full items-center h-12 px-4 gap-4">
+                <input
+                  type="radio"
+                  value="repayment"
+                  name="mortgageType"
+                  id="repayment"
+                  checked={formData.mortgageType === "repayment"}
+                  onChange={handleChange}
+                />
+                <label htmlFor="repayment" className="font-bold cursor-pointer">
+                  Repayment
+                </label>
+              </div>
+              <div className="flex border rounded-md w-full items-center h-12 px-4 gap-4">
+                <input
+                  type="radio"
+                  value="interest-only"
+                  name="mortgageType"
+                  id="interest-only"
+                  checked={formData.mortgageType === "interest-only"}
+                  onChange={handleChange}
+                />
+                <label
+                  htmlFor="interest-only"
+                  className="font-bold cursor-pointer"
+                >
+                  Interest Only
+                </label>
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              className="flex items-center text-lg font-bold justify-center cursor-pointer bg-[#D8DB2F] rounded-full py-4 text-black"
+            >
+              <img
+                src="src/assets/images/icon-calculator.svg"
+                alt="calculator icon"
+                className="mr-3"
+              />
+              Calculate Repayments
+            </button>
+          </div>
+        </form>
+
+        {!hasResults ? (
+          <div className="h-full flex flex-col justify-center items-center gap-4 bg-slate-900 text-white p-6 rounded-md">
             <img
-              src="src/assets/images/icon-calculator.svg"
-              alt="calculator icon"
-              className="mr-3"
+              src="src/assets/images/illustration-empty.svg"
+              alt="empty results"
+              className="w-[192px]"
             />
-            Calculate Repayments
-          </button>
-        </div>
-      </form>
-      {!hasResults ? (
-        <div className="gap-4 bg-slate-900 flex flex-col text-white justify-center text-center items-center p-6 rounded-md">
-          <img
-            src="src/assets/images/illustration-empty.svg"
-            alt="empty results"
-            className="w-[192px]"
-          />
-          <h2 className="text-2xl font-bold">Results Shown here</h2>
-          <p className="text-slate-300 text-base font-medium">
-            Complete the form and click “calculate repayments” to see what your
-            monthly repayments would be.
-          </p>
-        </div>
-      ) : (
-        //replace hardcoded numbers with dynamic values
-        <div className="gap-4 bg-slate-900 flex flex-col text-white  px-6 py-8 rounded-md">
-          <h3 className="text-2xl font-bold">Your Results</h3>
-          <p className="text-slate-300">
-            Your results are shown below based on the information you provided.
-            To adjust the results, edit the form and click “calculate
-            repayments” again.
-          </p>
-          <div className="mt-6 border-t bg-[#000000]/25 border-t-[#D8DB2F] border-t-4 py-4 px-6 rounded-lg">
-            <p className="text-slate-300">Your monthly repayments</p>
-            <p className="text-[40px] text-[#D8DB2F] font-bold">$1,787.74</p>
-            <hr className="mt-4 mb-4 opacity-25 fill-[#9ABED5]" />
-            <p className="text-slate-300">Total you'll repay over the term</p>
-            <p className="text-2xl mt-2 font-bold">$539,322.94</p>
+            <h2 className="text-2xl font-bold">Results Shown here</h2>
+            <p className="text-slate-300 text-base font-medium text-center">
+              Complete the form and click “calculate repayments” to see what
+              your monthly repayments would be.
+            </p>
           </div>
-        </div>
-      )}
+        ) : (
+          <div className="h-full flex flex-col gap-4 bg-slate-900 text-white px-6 py-8 rounded-md">
+            <h3 className="text-2xl md:text-3xl font-bold">Your Results</h3>
+            <p className="text-slate-300">
+              Your results are shown below based on the information you
+              provided. To adjust, edit the form and click “calculate
+              repayments” again.
+            </p>
+            <div className="mt-6 bg-[#000000]/25 border-t-[#D8DB2F] border-t-4 py-4 px-6 rounded-lg">
+              <p className="text-slate-300">Your monthly repayments</p>
+              <p className="text-[40px] md:text-[48px] lg:text-[56px] text-[#D8DB2F] font-bold">
+                ${results.toFixed(2)}
+              </p>
+              <hr className="mt-4 mb-4 opacity-25 fill-[#9ABED5]" />
+              <p className="text-slate-300">Total you'll repay over the term</p>
+              <p className="text-2xl md:text-3xl mt-2 font-bold">
+                ${totalRepayment.toFixed(2)}
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
